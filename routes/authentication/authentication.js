b1cbe903c162ba12cc5d6ae60ac60f0e8dba0d7a
Robinhood <okaforchidubem7@gmail.com>
2023-08-09 23:13:54 +0100

    added login route

---
+++ routes/authentication/authentication.js
@@ -0,0 +1,5 @@
+const { router } = require("express");
+
+const aut = router();
+
+aut.get("");
--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const { connection } = require("./connect.js");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
 require("dotenv").config();
+
 const PORT = process.env.PORT || 8090;
 const app = express();
 
@@ -17,38 +20,125 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
-app.post("/api/signup", (req, res) => {
+app.post("/api/signup", async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
     res
       .status(400)
-      .json({ message: "not complete", type: "error", status: "404" });
+      .json({ message: "not complete", type: "error", status: 404 });
   }
-  sql = "INSERT INTO createaccount(username,passwords,email) VALUES(?,?,?)";
-  values = [name, password, email];
 
-  connection.query(sql, values, (error, response) => {
+  try {
+    const encrypted_password = await bcrypt.hash(
+      password,
+      process.env.SALTROUNDS
+    );
+
+    sql = "INSERT INTO createaccount(username,passwords,email) VALUES(?,?,?)";
+    values = [name, encrypted_password, email];
+
+    connection.query(sql, values, (error, result) => {
+      if (error) {
+        console.error("reverted with: ", error);
+        res.status(500).json({
+          message: "internal server error",
+          type: "sql error",
+          status: "500",
+        });
+      } else {
+        const token = jwt.sign(
+          { name, email, encrypted_password },
+          process.env.SECRET,
+          { expiresIn: "1h" }
+        );
+        res.cookie("token", token, {
+          httpOnly: true,
+        });
+        res.status(200).json({ message: "account created", status: 200 });
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "internal server error",
+      type: "hashing error",
+      status: 500,
+    });
+  }
+});
+
+app.post("/api/login", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(401).json({
+      message: "incomplete data",
+      type: "error",
+      status: 401,
+    });
+  }
+
+  const sql =
+    "SELECT passwords,username,email FROM createaccount WHERE email = ?";
+  value = [email];
+
+  connection.query(sql, value, async (error, result) => {
     if (error) {
-      console.error("reverted with: ", error);
+      console.log("reverted with: ", error);
       res.status(500).json({
         message: "internal server error",
-        type: "sql error",
-        status: "500",
+        type: "query error",
+        status: 500,
       });
-    } else {
-      res.status(200).json({ message: "account created", status: "200" });
+    }
+
+    if (result == 0) {
+      console.log("does not exist");
+      res.status(404).json({
+        message: "account does not exist",
+        type: "",
+        status: 404,
+      });
+    }
+
+    const encrypted_password = result[0].passwords;
+    const DBusername = result[0].username;
+    const DBemail = result[0].email;
+    try {
+      const passwordMatch = await bcrypt.compare(password, encrypted_password);
+
+      if (passwordMatch) {
+        const token = jwt.sign(
+          { DBusername, DBemail, encrypted_password },
+          process.env.SECRET,
+          {
+            expiresIn: "1h",
+          }
+        );
+
+        res.cookie("token", token, {
+          httpOnly: true,
+        });
+        res.status(200).json({ message: "login successfull", status: 200 });
+      } else {
+        console.log("incorrect password");
+        res.status(401).json({
+          message: "incorrect password",
+          type: "unAuthorized",
+          status: 401,
+        });
+      }
+    } catch (erro) {
+      console.error(error);
+      res.status(500).json({
+        message: "internal server erro",
+        type: "server returned error",
+        status: 500,
+      });
     }
   });
 });
-
-// app.get("/api/login", (req, res) => {
-//   if() {
-
-//   } else {
-
-//   }
-// });
 
 app.listen(PORT, () => {
   console.log(`app listening on port ${PORT}...`);
